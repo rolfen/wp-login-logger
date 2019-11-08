161a047647757eef8e0fdf5f644849e8d3c52b27
rolfen <r10@eebe.be>
2019-11-08 19:32:23 +0000

    move logfile, document functions, normalize formatting, tell git to ignore logfile

--- login-logger.php
+++ login-logger.php
@@ -11,11 +11,15 @@
 
 add_action('wp_login', 'log_details', 10, 2);
 
+/**
+ * Get IP of logged in user
+ * @returns {string} IP
+ */
 function get_user_ip() {
-	if ( ! empty( $_SERVER['HTTP_CLIENT_IP'] ) ) {
+	if(!empty($_SERVER['HTTP_CLIENT_IP'])) {
 		//check ip from share internet
 		$ip = $_SERVER['HTTP_CLIENT_IP'];
-	} elseif ( ! empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
+	} elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
 		//to check ip is pass from proxy
 		$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 	} else {
@@ -24,7 +28,11 @@ function get_user_ip() {
 	return $ip;
 }
  
-
+/**
+ * Callback for the wp_login action
+ * @param {string} $user_login - The WordPress username of the logged in user
+ * @param {object} $user - The WP_User object of the logged in user
+ */
 function log_details($user_login, $user) {
 	append_to_log(
 		array(
@@ -35,10 +43,15 @@ function log_details($user_login, $user) {
 	);
 }
 
-function append_to_log($message) {
-	// $message is an array
-	$file = fopen("./login.log","a"); 
-	fwrite($file, date('Y-m-d h:i:s') . ", " . implode(', ', $message) . "\n"); 
+/**
+ * Appends a single-line record to the login log. Fields will be comma-separated
+ * The log file will be in the root folder of the plugin.
+ * @param {array} $record - The log record as an array of fields
+ */
+function append_to_log($record) {
+	$log_path = plugin_dir_path(__FILE__) . "login.log";
+	$file = fopen($log_path, 'a'); 
+	fwrite($file, date('Y-m-d h:i:s') . ", " . implode(', ', $record) . "\n"); 
 	fclose($file); 
 }
 
